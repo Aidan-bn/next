bb83a10e506bf940ce0822d3f3d804a00152f7fa
Aidan-bn <bantezea@gmail.com>
2026-01-22 14:33:39 +0300

    Working on pagination property

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -22,16 +22,17 @@ export default function Search({ placeholder }: { placeholder: string }) {
   //   replace(`${pathname}?${params.toString()}`);
   // }, 300);
   const handleSearch = useDebouncedCallback((term) => {
-    console.log(`Searching... ${term}`);
-   
+   console.log('Searching for:', `${term}`);
+
     const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
     if (term) {
       params.set('query', term);
     } else {
       params.delete('query');
     }
     replace(`${pathname}?${params.toString()}`);
-  }, 300);
+  }, 700);
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
